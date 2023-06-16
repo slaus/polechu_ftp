17a96ab4de1c6bbd078807c1ac817f26391473ce
Artem Romaniuk <artem.romaniuk@gmail.com>
2023-06-16 15:05:14 +0300

    Search tours init

--- helpers/images.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (! function_exists('image_uri')) {
-
-    function image_uri($path, $template = 'original', $absolute = true): string {
-        return route('imagecache', ['template' => $template, 'filename' => trim($path, '/')], $absolute);
-    }
-
-}
