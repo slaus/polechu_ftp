4886a1650e269c54b1d119cb96f0d25a5b263e25
Artem Romaniuk <artem.romaniuk@gmail.com>
2023-07-17 16:04:00 +0300

    Google script

--- resources/views/web/layouts/main.blade.php
+++ resources/views/web/layouts/main.blade.php
@@ -83,6 +83,16 @@
 
     @stack('meta')
     @stack('styles')
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-K5V82FJLBW"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-K5V82FJLBW');
+    </script>
 </head>
 <body>
 <div class="bg-preloader-white"></div>
