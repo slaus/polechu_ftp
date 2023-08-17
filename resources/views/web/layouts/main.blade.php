e36e7b354d6e2542d5d0c9402e3da8ecaf665fd3
Artem Romaniuk <artem.romaniuk@gmail.com>
2023-08-17 09:50:21 +0300

    Added fb pixel

--- resources/views/web/layouts/main.blade.php
+++ resources/views/web/layouts/main.blade.php
@@ -336,7 +336,7 @@
 <script src="{{ asset('web/rs-plugin/js/jquery.themepunch.revolution.min.js') }}"></script>
 <script src="{{ asset('web/js/on3step.js') }}"></script>
 <script src="{{ asset('web/js/plugin-set.js') }}"></script>
-<script src="{{ asset('js/app.js') }}?v=1"></script>
+<script src="{{ asset('js/app.js') }}?v=2"></script>
 
 @stack('scripts')
 </body>
