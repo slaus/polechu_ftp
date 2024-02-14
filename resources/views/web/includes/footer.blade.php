ea305958c95393b1b8752ac6630522ca1294ed98
Mykyta Shulha <mykyta.shulha@pandateam.net>
2024-02-14 06:35:34 +0200

    UL4U-16 add new feature

--- resources/views/web/includes/footer.blade.php
+++ resources/views/web/includes/footer.blade.php
@@ -1,9 +1,5 @@
 <style>
-<<<<<<< HEAD
     /* menu nav */
-=======
-/* menu nav */
->>>>>>> e68556fd4b23552bae31fdd26c460bdf0f371f14
 .footer-white nav {
   position: relative;
   margin-right:18px;
@@ -20,10 +16,6 @@
 }
 .footer-white nav ul li {
   margin: 0;
-<<<<<<< HEAD
-=======
-  margin-right:-4px;
->>>>>>> e68556fd4b23552bae31fdd26c460bdf0f371f14
 }
 .footer-white nav ul li .input-group {
   top:15px;
@@ -73,11 +65,7 @@
   -webkit-transition: max-width 0.5s;
   -moz-transition: max-width 0.5s;
   transition: max-width 0.5s;
-<<<<<<< HEAD
   footer-white-space: nowrap;
-=======
-  white-space: nowrap;
->>>>>>> e68556fd4b23552bae31fdd26c460bdf0f371f14
 }
 .footer-white nav a.actived {
   color: #ccc !important;
@@ -87,10 +75,6 @@
   display: none;
   position: absolute;
   color: #efefef;
-<<<<<<< HEAD
-=======
-  z-index: 9999;
->>>>>>> e68556fd4b23552bae31fdd26c460bdf0f371f14
 }
 .footer-white nav ul li:hover>ul {
   display: inherit;
@@ -203,16 +187,6 @@ li a.btn-book-menu:hover {
                 @endif
             </div>
 
-
-            <div class="footer-white col-md-4 col-xs-12 col-md-offset-1">
-                <x-menu-component layout="main" searchButton="false" />
-
-                <div class="title-h3 title-h">{!! translation(settings('contacts.title', [])) !!}</div>
-                <address>
-                    {!! translation(settings('contacts.text', [])) !!}
-                </address>
-            </div>
-
             <div class="footer-white col-md-4 col-xs-12 col-md-offset-1">
                 <x-menu-component layout="main" searchButton="false" />
             </div>
@@ -220,13 +194,9 @@ li a.btn-book-menu:hover {
             <div class="col-md-3 col-xs-12 pull-right">
                 <div class="title-h3 title-h">{!! translation(settings('subscribe.title', [])) !!}</div>
                 <div>
-<<<<<<< HEAD
                     @if(! empty(settings('subscribe.description', [])))
                         {!! translation(settings('subscribe.description', [])) !!}
                     @endif
-=======
-                    {!! translation(settings('subscribe.description', [])) !!}
->>>>>>> e68556fd4b23552bae31fdd26c460bdf0f371f14
 {{--                    <form action="{{ route('form.feedback') }}" id="feedbackForm" method="post" name="subscribe">--}}
 {{--                        <div>--}}
 {{--                            <input class="subscribfield subscribeemail" id="subscribeemail" name="email" type="text" placeholder="{{ translation(settings('subscribe.email_placeholder', [])) }}">--}}
